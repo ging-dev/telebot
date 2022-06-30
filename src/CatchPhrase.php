b96cd9a4a0b584fd39c80878313d46daadc713dd
ging-dev <thanh1101dev@gmail.com>
2022-06-30 17:32:49 +0700

    make non-blocking event-loop

--- src/CatchPhrase.php
+++ src/CatchPhrase.php
@@ -1,20 +1,24 @@
 <?php
 
-use function React\Async\await;
+use function Amp\asyncCall;
+use React\Promise\PromiseInterface;
 use function Symfony\Component\String\u;
 use Zanzara\Context;
 
+/**
+ * @psalm-type QuestionType = array{image: string, result: string}
+ */
 class CatchPhrase
 {
-    /** @var list<array{image: string, result: string}> */
+    /** @var list<QuestionType> */
     private static array $data = [];
 
-    /** @var array<string, array{image: string, result: string}> */
+    /** @var array<string, QuestionType> */
     private static array $current = [];
 
     public const IMAGE_URL = 'https://e.gamevui.vn/web/2014/10/batchu/assets/pics/';
 
-    /** @param list<array{image: string, result: string}> $data */
+    /** @param list<QuestionType> $data */
     public static function importData(array $data): void
     {
         self::$data = $data;
@@ -23,49 +27,51 @@ class CatchPhrase
     /**
      * Chuyển câu hỏi.
      */
-    public static function changeQuestion(Context $ctx): void
+    public static function changeQuestion(Context $ctx): PromiseInterface
     {
         $question = array_shift(self::$data);
 
         if (null === $question) {
-            $ctx->sendMessage('Đã hết câu hỏi.');
-
-            return;
+            return $ctx->sendMessage('Đã hết câu hỏi.');
         }
 
         self::$current[getGroupId($ctx)] = $question;
 
-        $ctx->sendPhoto(self::IMAGE_URL.$question['image']);
+        return $ctx->sendPhoto(self::IMAGE_URL.$question['image']);
     }
 
     public function game(Context $ctx): void
     {
-        self::changeQuestion($ctx);
+        self::changeQuestion($ctx)->then(function () use ($ctx) {
+            $ctx->sendMessage('Bắt đầu Game!');
+        });
     }
 
     public function answer(Context $ctx, string $text): void
     {
-        $current = self::$current[getGroupId($ctx)];
-        $opt = ['reply_to_message_id' => $ctx->getMessage()?->getMessageId()];
+        asyncCall(function () use ($ctx, $text) {
+            $current = self::$current[getGroupId($ctx)];
+            $opt = ['reply_to_message_id' => $ctx->getMessage()?->getMessageId()];
 
-        if ('skip' === $text) {
-            await($ctx->sendMessage('Đáp án: '.$current['result']));
+            if ('skip' === $text) {
+                yield $ctx->sendMessage('Đáp án: '.$current['result']);
 
-            self::changeQuestion($ctx);
-        } elseif (u($text)->ascii()->lower() == u($current['result'])->ascii()->lower()) {
-            $name = $ctx->getMessage()?->getFrom()->getFirstName();
+                self::changeQuestion($ctx);
+            } elseif (u($text)->ascii()->lower() == u($current['result'])->ascii()->lower()) {
+                $name = $ctx->getMessage()?->getFrom()->getFirstName();
 
-            if (5214954937 === $ctx->getMessage()?->getFrom()->getId()) {
-                $message = 'Chồng yêu đã trả lời chính xác! 😍';
+                if (5214954937 === $ctx->getMessage()?->getFrom()->getId()) {
+                    $message = 'Chồng yêu đã trả lời chính xác! 😍';
+                } else {
+                    $message = sprintf('Bạn %s đã trả lời chính xác! 😊', $name);
+                }
+
+                yield $ctx->sendMessage($message, $opt);
+
+                self::changeQuestion($ctx);
             } else {
-                $message = sprintf('Bạn %s đã trả lời chính xác! 😊', $name);
+                $ctx->sendMessage('Không chính xác...', $opt);
             }
-
-            await($ctx->sendMessage($message, $opt));
-
-            self::changeQuestion($ctx);
-        } else {
-            $ctx->sendMessage('Không chính xác...', $opt);
-        }
+        });
     }
 }
